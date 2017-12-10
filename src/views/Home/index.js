f91ac0799eebd176f4841b17b802af25b5556ab1
david <395603360@qq.com>
2017-12-10 14:51:07 +0800

    updata

---
+++ src/views/Home/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello world!
+      </div>
+    );
+  }
+}
+export default Home;
