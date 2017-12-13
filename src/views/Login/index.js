4f12d10ec510544ccca675e29bbca8aec66aeeba
david <395603360@qq.com>
2017-12-13 23:18:03 +0800

    Update

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import {
   Link
 } from 'react-router-dom';
+import { message, Input, Icon } from 'antd';
+import styles from './index.less'
 class Login extends React.Component {
+
+
+
   render() {
     return (
-      <div>
+      <div className={styles['login-back']} style={{background:'blue'}}>
         this is Login!
         <Link to='/index'>首页</Link>
+        <br/>
+        <Icon type="step-forward" />
+        <br/>
+        <Input placeholder="Basic usage" />
+        this is time {new Date().toLocaleTimeString()}
       </div>
     );
   }
